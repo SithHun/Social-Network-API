e12bd35987f66ae9f0ffa64c49c9aa0ebcd826d5
Seth Sun <sithsun@gmail.com>
2023-06-23 02:22:21 -0400

    re-coded userController with empty method using async operation with a try and catch block for all necessary userRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,60 @@
 const User = require('../models/User');
+const Thought = require('../models/Thought');
 
 module.exports = {
-  async getUsers(req, res) {
+  async getAllUsers(req, res) {
     try {
-      const users = await User.find();
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
+      // Code for retrieving all users
+    } catch (error) {
+      // Error handling
     }
   },
-  async getSingleUser(req, res) {
+
+  async getUserById(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v');
-
-      if (!user) {
-        return res.status(404).json({ message: 'No user with that ID' });
-      }
-
-      res.json(user);
-    } catch (err) {
-      res.status(500).json(err);
+      // Code for retrieving a single user by id
+    } catch (error) {
+      // Error handling
     }
   },
-  // create a new user
+
   async createUser(req, res) {
     try {
-      const dbUserData = await User.create(req.body);
-      res.json(dbUserData);
-    } catch (err) {
-      res.status(500).json(err);
+      // Code for creating a new user
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      // Code for updating a user
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      // Code for deleting a user
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async addFriend(req, res) {
+    try {
+      // Code for adding a friend
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async removeFriend(req, res) {
+    try {
+      // Code for removing a friend
+    } catch (error) {
+      // Error handling
     }
   },
 };
