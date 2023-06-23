060148d40b51cf2ade23a3235649951efbd1c29a
Seth Sun <sithsun@gmail.com>
2023-06-23 02:32:12 -0400

    Coded an empty method with async operation with try and catch block for thoughtController.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,60 @@
+const Thought = require('../models/Thought');
+const User = require('../models/User');
+
+module.exports = {
+  async getAllThoughts(req, res) {
+    try {
+      // Code for retrieving all thoughts
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async getThoughtById(req, res) {
+    try {
+      // Code for retrieving a single thought by id
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async createThought(req, res) {
+    try {
+      // Code for creating a new thought
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async updateThought(req, res) {
+    try {
+      // Code for updating a thought
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      // Code for deleting a thought
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async createReaction(req, res) {
+    try {
+      // Code for creating a reaction
+    } catch (error) {
+      // Error handling
+    }
+  },
+
+  async deleteReaction(req, res) {
+    try {
+      // Code for deleting a reaction
+    } catch (error) {
+      // Error handling
+    }
+  },
+};
