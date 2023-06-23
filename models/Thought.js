9fec111de30cc1ce9c5c94f048cd79c448306d96
Seth Sun <sithsun@gmail.com>
2023-06-23 00:51:14 -0400

    Created thought schema

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('timeStamp').get(function (){
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    return this.createdAt.toLocaleDateString(undefined, options);
+})
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
